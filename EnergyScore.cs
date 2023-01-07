10908dd54a0bbc4445bf0f0b8336514fdd86c240
max <maxbastienbalej@gmail.com>
2023-01-07 22:35:46 +0100

    adding reducer force when player do not catch points

---
+++ EnergyScore.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyScore : MonoBehaviour
+{
+
+
+    public float reducer = 0f;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
--- PlayerController.cs
+++ PlayerController.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D playerRigidbody;
-    public float maxPushForce = 1000.0f;
-    public float minDragDistance = 0.1f;
-    public float maxDragDistance = 20f;
+
+    EnergyScore energyManager;
+    float maxPushForce = 2500.0f;
+    float minDragDistance = 0.1f;
+    float maxDragDistance = 20f;
 
     Vector2 startPosition;
+    float forceReducer;
 
-    void Start()
+
+    void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        energyManager = GetComponent<EnergyScore>();
     }
 
     void Update()
     {
+        MovePlayer();
+    }
+
+
+    void MovePlayer()
+    {
+
         if (Input.GetMouseButtonDown(0))
         {
             startPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -31,9 +43,25 @@ public class PlayerController : MonoBehaviour
 
             float pushForce = Mathf.Lerp(0, maxPushForce, (dragDistance - minDragDistance) / (maxDragDistance - minDragDistance));
 
-            Debug.Log(pushForce);
-            // Apply the push force to the player
-            playerRigidbody.AddForce(new Vector2(-dragDirection.x, Mathf.Abs(dragDirection.y)) * pushForce, ForceMode2D.Force);
+            IncreaseMassByDragDistance(dragDistance);
+
+            playerRigidbody.AddForce(new Vector2(-dragDirection.x, Mathf.Abs(dragDirection.y)) * (pushForce), ForceMode2D.Force);
         }
+
+    }
+
+
+    void IncreaseMassByDragDistance(float dragDistance)
+    {
+
+        if (playerRigidbody.mass >= 3f)
+        {
+            playerRigidbody.mass = 50f;
+            return;
+        }
+
+        float NewEnergy = energyManager.reducer + dragDistance;
+        playerRigidbody.mass = 1 + (NewEnergy / 100);
+        energyManager.reducer = NewEnergy;
     }
 }
